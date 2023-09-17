318dc7be863dc03b9f7a1ff0331143f2e8eae15a
Pharit Smitasin <smitasinpharit@yahoo.com>
2023-09-17 03:57:43 -0400

    Revert "past typescript files commented out"
    This reverts commit c8ce206ba56f9f5db3662d6c872da5b0e0644cc8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-// import { useState } from "react";
-// import "./App.css";
+import { useState } from "react";
+import "./App.css";
 
-// function App() {
-//   const [result, setResult] = useState(null);
+function App() {
+  const [result, setResult] = useState(null);
 
-//   return (
-//     <>
-//       <h1 className="title">
-//         Sustainable <span className="strike">500</span>
-//       </h1>
-//       <p className="subheading">
-//         Dive into a web application that rates the economic sustainability of
-//         every company on the <b>S&P 500</b>. With sleek visualizations and
-//         comprehensive data analytics, Sustainable 500 allows you to make
-//         informed investment decisions that align with your financial goals.
-//       </p>
-//       <p className="user-instructions">
-//         Enter the ticker for the company you would like to learn about:
-//       </p>
-//       <div className="card">
-//         {/* <button onClick={() => setCount((count) => count + 1)}>
-//           LEARN MORE
-//         </button> */}
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h1 className="title">
+        Sustainable <span className="strike">500</span>
+      </h1>
+      <p className="subheading">
+        Dive into a web application that rates the economic sustainability of
+        every company on the <b>S&P 500</b>. With sleek visualizations and
+        comprehensive data analytics, Sustainable 500 allows you to make
+        informed investment decisions that align with your financial goals.
+      </p>
+      <p className="user-instructions">
+        Enter the ticker for the company you would like to learn about:
+      </p>
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          LEARN MORE
+        </button> */}
+      </div>
+    </>
+  );
+}
 
-// export default App;
+export default App;
